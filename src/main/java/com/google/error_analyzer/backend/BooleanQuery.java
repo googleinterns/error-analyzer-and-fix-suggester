c3428392a52d6da54a2198ae680ed9eec52948c1
rythmag <rythmagarwal0831@gmail.com>
2020-06-15 19:43:56 +0000

    Change structure of ErrorResults

--- src/main/java/com/google/error_analyzer/backend/BooleanQuery.java
+++ src/main/java/com/google/error_analyzer/backend/BooleanQuery.java
@@ -1,151 +1,72 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**Copyright 2019 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 
 package com.google.error_analyzer.backend;
 
-import com.google.error_analyzer.data.Keywords;
-import com.google.error_analyzer.data.ErrorLine;
-import com.google.error_analyzer.data.RegexExpressions;
-import com.google.gson.Gson;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-import java.io.IOException;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.RegexpQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import java.util.*;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import com.google.error_analyzer.data.Keywords;
+import com.google.error_analyzer.data.RegexExpressions;
+import org.elasticsearch.index.query.RegexpQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+ 
 public class BooleanQuery {
     private static final Logger logger = LogManager.getLogger(BooleanQuery.class);
+    private final String logTextField = "logText";
 
-    private String logTextField = "logText"; //subject to field name in the document used while storing.
-    private String logLineNumberField = "logLineNumber"; //subject to field name in the document used while storing.
-
-    //returns an Errorline object array as a json string. 
-    public String getErrorsAsString(String indexFile, RestHighLevelClient client){
-        try{
-            ArrayList<String> errorData = sortSearchhits(indexFile,client);
-            Gson gson = new Gson();
-            String json = gson.toJson(errorData);
-            return json;
-        }catch(IOException e){
-            logger.error("Could not complete query request: "+e);
-            return "Could not complete request.";
-        }
-
-    }
-
-    //returns all search hits as Errorline object array
-    public ArrayList<ErrorLine> getErrors(String indexFile, RestHighLevelClient client) throws IOException{
-        ArrayList<ErrorLine> errorData = new ArrayList<>();
-        SearchHits hits = getQueryHits(indexFile,client);
-        errorData = getLogData(hits);
-        return errorData;
-    }
-
-    //Make the search request and  return Search hits from match query.
-    private SearchHits getQueryHits(String indexFile, RestHighLevelClient client) throws IOException{
-        SearchRequest searchRequest = new SearchRequest(indexFile);
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder(); 
-
-        // kewords class contains all the terms used combined in OR logic.
+    //search db using regex and keywords and store back in db searchHits sorted by logLineNumber
+    public SearchRequest createSearchRequest(String fileName) {
+        SearchRequest searchRequest = new SearchRequest(fileName);
         Keywords errorKeywords = new Keywords();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         RegexExpressions regexExpressions = new RegexExpressions();
-        
-        RegexpQueryBuilder regexQuery = new RegexpQueryBuilder(logTextField, regexExpressions.getQueryString() ); 
-        QueryBuilder fulltextQuery = QueryBuilders.matchQuery(logTextField, errorKeywords.getQueryString() );
-        QueryBuilder qb = new BoolQueryBuilder()
+        String regexQueryString = regexExpressions.getQueryString();
+        RegexpQueryBuilder regexQuery = new RegexpQueryBuilder(logTextField,regexQueryString);
+        String keywordsQueryString = errorKeywords.getQueryString();
+        QueryBuilder fulltextQuery = QueryBuilders.matchQuery(logTextField, keywordsQueryString);
+        QueryBuilder errorQuery = new BoolQueryBuilder()
             .minimumShouldMatch(1)
             .should(regexQuery)
             .should(fulltextQuery);
-        searchSourceBuilder.query(qb); 
-        searchRequest.source(searchSourceBuilder); 
-
-        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-        SearchHits hits = searchResponse.getHits();
-        return hits;
+        searchSourceBuilder.query(errorQuery); 
+        searchRequest.source(searchSourceBuilder);
+        return searchRequest;
     }
-
-    private ArrayList<String> sortSearchhits (String indexFile, RestHighLevelClient client) throws IOException {
-        SearchHits hits = getQueryHits(indexFile,client);
+    
+    //Sort the searchHits acc to ids (which is also logLineNumber) and return document json strings
+    public ArrayList<String> sortErrorDocuments(SearchHits hits) {
         ArrayList<Integer> searchHitIds = new ArrayList<>();
         HashMap<Integer, String> hitsHashMap = new HashMap();
-        for(SearchHit hit : hits){
+        for (SearchHit hit : hits) {
             Integer id = Integer.parseInt(hit.getId());
             searchHitIds.add(id);
             String jsonDocument = hit.getSourceAsString();
             hitsHashMap.put(id, jsonDocument);
         }
-        ArrayList<String> sourceStrings = new ArrayList();
         Collections.sort(searchHitIds);
-        for(Integer id : searchHitIds){
-            sourceStrings.add(hitsHashMap.get(id));
+        ArrayList<String> sortedSourceStrings = new ArrayList();
+        for (Integer id : searchHitIds) {
+            String errorJsonString = hitsHashMap.get(id);
+            sortedSourceStrings.add(errorJsonString);
         }
-        return sourceStrings;
+        return sortedSourceStrings;
     }
-    // return log text from a hit.
-    private String extractLogText(SearchHit hit){
-        String sourceAsString = hit.getSourceAsString();
-        Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-        return (String) sourceAsMap.get(logTextField);
-    }
-
-    // return log line number from a hit 
-    private int extractLogLineNumber(SearchHit hit){
-        String sourceAsString = hit.getSourceAsString();
-        Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-        //subject to the format logLineNumber is stored in index, string or integer.
-        try{
-            int lineNumber = Integer.parseInt((String) sourceAsMap.get(logLineNumberField));
-            logger.info("Integer.parseInt successfully = " + lineNumber);
-            return lineNumber;
-        }catch(NumberFormatException e){
-            logger.error("Integer.parseInt error: " + e);
-            return -1;
-        }catch(NullPointerException e2){
-            logger.error("Null error: " + e2);
-            return -1;
-        }
-    }
-    
-    //Create Errorline array from search hits.
-    private ArrayList<ErrorLine> getLogData(SearchHits hits){
-
-        ArrayList<ErrorLine> errorMsgs = new ArrayList<>();
-        for (SearchHit hit : hits){
-            int lineNumber = extractLogLineNumber(hit);
-            if( lineNumber != -1){
-                errorMsgs.add(new ErrorLine( extractLogText(hit),lineNumber) );
-            }else{
-                logger.error("Could process log line.");
-            }
-        }
-        return errorMsgs;
-    }
-
 }
--- src/main/java/com/google/error_analyzer/servlets/ErrorResults.java
+++ src/main/java/com/google/error_analyzer/servlets/ErrorResults.java
@@ -28,28 +28,37 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
 
 @WebServlet("/errorResults")
 public class ErrorResults extends HttpServlet {
     private String indexFile = "trial_index"; //later fetched from request
     private static final Logger logger = LogManager.getLogger(ErrorResults.class);
     private String logTextField = "logText"; 
+    RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("35.194.181.238", 9200, "http")));
     
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
-        try{
-            RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("35.194.181.238", 9200, "http")));
+        try {
             BooleanQuery searchQuery = new BooleanQuery();
-
-            String errorData = searchQuery.getErrorsAsString(indexFile, client);
+            SearchRequest searchRequest = searchQuery.createSearchRequest(indexFile);
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits hits = searchResponse.getHits();
+            ArrayList<String> errorData = searchQuery.sortErrorDocuments(hits);
             response.getWriter().println(errorData);
-        }catch(Exception e){
-            logger.error("Could not connect to server." + e);
-            response.getWriter().println("Could not connect to database." );
+            String infoMsg = "Completed Boolean query successfully on ";
+            infoMsg = infoMsg.concat(indexFile);
+            logger.info(infoMsg);
+        } catch(IOException e) {
+            String loggerError = "Could not connect to database.";
+            loggerError = loggerError.concat(e.toString());
+            logger.error(loggerError);
+            response.getWriter().println(loggerError);
         }
-        
     }
 }
