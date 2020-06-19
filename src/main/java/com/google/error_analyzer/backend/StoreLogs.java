54373de32887af98279f6f96133010d10753ec8b
Astha <asthaatiwari@gmail.com>
2020-06-19 10:54:17 +0000

    Add TextServlet.java StoreLogs.java, StoreLogTest.java

---
+++ src/main/java/com/google/error_analyzer/backend/StoreLogs.java
@@ -0,0 +1,81 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.error_analyzer.backend;
+
+import com.google.error_analyzer.backend.LogDao;
+import java.io.IOException;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+//This class contains the methods used for processing logs and storing them to the database.
+//This includes removal of special characters, conversion into json string.
+public class StoreLogs {
+
+    private static final Logger logger = LogManager.getLogger(StoreLogs.class);
+    public LogDao logDao = new LogDao();
+
+    //convert the logText and logLineNumber to a json string
+    public String convertToJsonString(String logText, int logLineNumber) {
+        final String removeSpecialCharactersString = "[^a-zA-Z0-9:\"]";
+        final String space = " ";
+        logText = logText.replaceAll(removeSpecialCharactersString, space);
+        //remove special characters
+        final String doubleInvertedComma = "\"";
+        final String singleInvertedComma = "\'";
+        logText = logText.replaceAll(doubleInvertedComma, singleInvertedComma);
+        logText = logText.replaceAll("\\s+", " ");
+        //remove extra white space
+        String jsonString = String.format("{\"logLineNumber\":%1$s," +
+            "\"logText\":\"%2$s\"}", logLineNumber, logText);
+        return jsonString;
+    }
+
+    //Calls the method StoreLog if an index with name fileName does not 
+    //exist in db
+    public String checkAndStoreLog(String fileName, String log)
+    throws IOException {
+        if (logDao.fileExists(fileName) == true) {
+            logger.info("File already exists");
+            final String response =
+                "\t\t\t<h2> Sorry! the file already exists</h2>";
+            return (response);
+        } else {
+            final String response = storeLog(fileName, log);
+            logger.info("File Stored");
+            return (response);
+
+        }
+    }
+
+    //Stores the log in an index with name fileName
+    public String storeLog(String fileName, String log) throws IOException {
+        final String lineBreakString = "\\r?\\n";
+        int logLineNumber = 1;
+        String logLines[] = log.split(lineBreakString);
+        for (String logLine: logLines) {
+            if (!(logLine.equals(""))) {
+                String logLineNum = Integer.toString(logLineNumber);
+                String jsonString = convertToJsonString(logLine, logLineNumber);
+                logDao.storeLogLine(fileName, jsonString, logLineNum);
+                logLineNumber++;
+            }
+        }
+        final String response = "\t\t\t<h2> File Stored</h2>";
+        return response;
+    }
+
+
+
+}
---
+++ src/test/java/com/google/error_analyzer/backend/StoreLogTest.java
@@ -0,0 +1,130 @@
+/**Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package com.google.error_analyzer;
+
+import com.google.error_analyzer.backend.LogDao;
+import com.google.error_analyzer.backend.MockLogDao;
+import com.google.error_analyzer.backend.StoreLogs;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+
+/*this class contains tests for the methods used for 
+storing logs to the database*/
+public final class StoreLogTest {
+    private MockLogDao mockLogDao = null;
+
+    @Mock
+    LogDao logDao;
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @InjectMocks
+    StoreLogs storeLogs;
+
+    @Before
+    public void setUp() {
+        mockLogDao = new MockLogDao();
+        storeLogs = new StoreLogs();
+        storeLogs.logDao = logDao;
+    }
+
+    //store the log into the database when index with name same as the
+    //file name does not exist in the db
+    @Test
+    public void checkAndStoreLogTest() throws IOException {
+        String log = "error1";
+        String fileName = "samplefile";
+        boolean fileExists = mockLogDao.fileExists(fileName);
+        String jsonString = storeLogs.convertToJsonString(log, 1);
+        String storedLog = mockLogDao.storeLogLine(fileName, jsonString, "1");
+        when(logDao.storeLogLine(fileName, jsonString, "1")).
+        thenReturn(storedLog);
+        when(logDao.fileExists(fileName)).thenReturn(fileExists);
+        String expected = "\t\t\t<h2> File Stored</h2>";
+        String actual = storeLogs.checkAndStoreLog(fileName, log);
+        assertEquals(expected, actual);
+
+    }
+
+    //store the log into the database when index with name same as the
+    //file name already exist in the database
+    @Test
+    public void checkAndStoreLogInAlreadyExistingFileTest()
+    throws IOException {
+        String log = "error2";
+        String fileName = "samplefile1";
+        boolean fileExists = mockLogDao.fileExists(fileName);
+        String jsonString = storeLogs.convertToJsonString(log, 1);
+        String storedLog = mockLogDao.storeLogLine(fileName, jsonString, "1");
+        when(logDao.storeLogLine(fileName, jsonString, "1")).
+        thenReturn(storedLog);
+        when(logDao.fileExists(fileName)).thenReturn(fileExists);
+        storeLogs.checkAndStoreLog(fileName, log);
+        fileExists = mockLogDao.fileExists(fileName);
+        when(logDao.fileExists(fileName)).thenReturn(fileExists);
+        String expected = "\t\t\t<h2> Sorry! the file already exists</h2>";
+        String actual = storeLogs.checkAndStoreLog(fileName, log);
+        assertEquals(expected, actual);
+
+    }
+
+    //convert loglines to json Strings
+    @Test
+    public void convertToJsonStringTest() {
+        String actual = storeLogs.
+        convertToJsonString("Error1:\"index not found\"", 5);
+        String expected = new String(
+            "{\"logLineNumber\":5,\"logText\":\"Error1:'index not found'\"}");
+        assertEquals(expected, actual);
+    }
+
+    //test the removal of special characters from json string
+    @Test
+    public void RemoveSpecialCharactersTest() {
+        String actual = storeLogs.
+        convertToJsonString("Error1:\"^&index not found?/,*\"", 5);
+        String expected = new String(
+            "{\"logLineNumber\":5,\"logText\":\"Error1:' index not found '\"}");
+        assertEquals(expected, actual);
+    }
+
+    //test for storeLog method
+    @Test
+    public void storeLogTest() throws IOException {
+        String log = "error2";
+        String fileName = "samplefile2";
+        String actual = storeLogs.storeLog(fileName, log);
+        String expected = "\t\t\t<h2> File Stored</h2>";
+        assertEquals(expected, actual);
+    }
+
+}
