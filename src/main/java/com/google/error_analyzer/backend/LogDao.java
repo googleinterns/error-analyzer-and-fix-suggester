b668ecfd1f4ac28032b29ec59bdaaaf894ed909a
astha <asthaatiwari@gmail.com>
2020-06-24 22:23:58 +0530

    change comment

--- src/main/java/com/google/error_analyzer/backend/LogDao.java
+++ src/main/java/com/google/error_analyzer/backend/LogDao.java
@@ -140,7 +140,7 @@ public class LogDao implements DaoInterface {
     }
 
     //Stores the documents into the database by performing multiple indexing operations
-    //using bulk API
+    //in a single API call
     @Override
     public void bulkStoreLog(String fileName, ImmutableList < Document > documentList)
     throws IOException {
