0348e6631d179c949bf81289f2e8581867d3a7f5
astha <asthaatiwari@gmail.com>
2020-06-27 22:54:44 +0530

    change Fileservlet

--- src/main/java/com/google/error_analyzer/servlets/FileServlet.java
+++ src/main/java/com/google/error_analyzer/servlets/FileServlet.java
@@ -1,3 +1,16 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.google.error_analyzer.servlets;
 
 import com.google.error_analyzer.backend.FileLogs;
@@ -22,27 +35,33 @@ import javax.servlet.http.Part;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import org.apache.http.HttpHost;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @WebServlet("/uploadFile")
 @MultipartConfig
 /*This class is for storing the logs provided by the user 
 as files to the database*/
 public class FileServlet extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(FileServlet.class);
     public static final FileLogs fileLogs = new FileLogs();
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException {
-        response.setContentType(FileConstants.TEXT_HTML_CONTENT_TYPE);
-        Part filePart = request.getPart(LogFields.FILE);
-        InputStream fileContent = filePart.getInputStream();
-        String fileName = request.getParameter(LogFields.FILE_NAME);
-        request.getSession();
-        String status =
-            fileLogs.checkAndStoreFileLog(request, fileName, fileContent);
-        response.getWriter().println(status);
-        RequestDispatcher requestDispatcher =
-            request.getRequestDispatcher(PageConstants.LANDING_PAGE);
-        requestDispatcher.include(request, response);
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType(FileConstants.TEXT_HTML_CONTENT_TYPE);
+            Part filePart = request.getPart(LogFields.FILE);
+            InputStream fileContent = filePart.getInputStream();
+            String fileName = request.getParameter(LogFields.FILE_NAME);
+            request.getSession();
+            String status =
+                fileLogs.checkAndStoreFileLog(request, fileName, fileContent);
+            response.getWriter().println(status);
+            RequestDispatcher requestDispatcher =
+                request.getRequestDispatcher(PageConstants.LANDING_PAGE);
+            requestDispatcher.include(request, response);
+        } catch (Exception e) {
+            logger.error("Could not store file", e);
+        }
     }
 }
