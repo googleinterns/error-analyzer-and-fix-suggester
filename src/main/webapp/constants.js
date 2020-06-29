8240ffea7239fadd27614c87da1e9981fc2bd4d7
kirti-d <kirtidabas@google.com>
2020-06-29 11:05:04 +0000

    add charts.js,constant and variable file

---
+++ src/main/webapp/constants.js
@@ -0,0 +1,53 @@
+/**Copyright 2019 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+// file contains all the constants used by all the js files
+
+// DOM elements
+const STACK_TRACE_CONTAINER = "stackTraceContainer";
+const SLIDE_1_CONTAINER = "slide1Container";
+const SLIDE_2_CONTAINER = "slide2Container";
+const SEARCH_BAR ="searchBar";
+const PIE_CHART= "piechart";
+const FILE_NAME = "fileName";
+const CROSS = "crossBtn";
+const PREVIOUS_BUTTON = "btnPrev";
+const NEXT_BUTTON = "btnNext";
+const SLIDE_1 = "slide1";
+const SLIDE_2 = "slide2";
+const PAGE = "page";
+
+// classes for dom status
+const ACTIVE = "active";
+const HIDE = "hide";
+const SHOW = "show";
+
+// attributes for dom status
+const HIDDEN = "hidden";
+const VISIBLE = "visible";
+
+// common constants for various js files
+const LOGS = "logs" ;
+const ERRORS = "errors";
+const FILE_TYPE = "fileType";
+const START = "start";
+const SIZE = "size";
+const LOG_LINE_NUMBER = "logLineNumber";
+const RECORDS = "records";
+const END = "end";
+const FILE_NOT_FOUND = "File Not Found";
+const SEARCH_STRING = "searchString";
+
+// servlet constants
+const POST = "POST";
+const GET_COUNT = "/getCount";
+const STACK_TRACE = "/stackTrace";
+const PAGINATION= "/pagination";
