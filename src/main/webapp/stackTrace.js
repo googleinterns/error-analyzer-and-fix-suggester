17bb1a746b359594e98f2f2eac628a7f49e6db92
kirti-d <kirtidabas@google.com>
2020-06-30 04:24:54 +0000

    change name to stackTraceButton

--- src/main/webapp/stackTrace.js
+++ src/main/webapp/stackTrace.js
@@ -9,8 +9,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
-// add button for stack trace
-addStackTraceButton = (logError) => {
+// return stack trace button
+StackTraceButton = (logError) => {
     const stackTraceButton = document.createElement(BUTTON);
     stackTraceButton.innerText="Stack Trace";
     stackTraceButton.className = "stackTraceButton";
