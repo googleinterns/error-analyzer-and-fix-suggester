934dcb991b27c09b8d02746965b4bac8ef36e787
rythmag <rythmagarwal0831@gmail.com>
2020-07-01 13:32:23 +0000

    Change findAndStoreErrors in MockLogDao

--- src/main/java/com/google/error_analyzer/backend/StoreLogs.java
+++ src/main/java/com/google/error_analyzer/backend/StoreLogs.java
@@ -53,7 +53,7 @@ public class StoreLogs {
             indexName = getUniqueIndexName(indexName);
             final String response = storeLog(
                 request, indexName, log, OFFSET_FOR_PLAIN_TEXT );
-            findErrorsInIndex(indexName);
+            findAnsStoreErrorsInIndex(indexName);
             return response;
         } catch (Exception e) {
             final String errorResponse =
@@ -110,7 +110,7 @@ public class StoreLogs {
         return nextIndexName;
     }
 
-    public void findErrorsInIndex(String indexName) {
+    public void findAnsStoreErrorsInIndex(String indexName) {
         try {
             TimeUnit.SECONDS.sleep(1);
             logDao.findAndStoreErrors(indexName);
